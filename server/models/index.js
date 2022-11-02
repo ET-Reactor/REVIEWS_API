ca383dbdba2df9033fb950483890ed7c1d6f6d46
Roshan Patel <roshanupatel@gmail.com>
2022-11-02 10:57:42 -0700

    Scaffolding for REVIEWS_API (routes, models, and controllers)

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,25 @@
 const db = require('../db/db.js');
 
-module.exports = {};
+module.exports = {
+
+  getReview: function(id, page, count, sort) {
+    // mongoDB/postgreSQL queries
+  }
+
+  getMeta: function(id) {
+    // mongoDB/postgreSQL queries
+  }
+
+  addReview: function (obj) {
+    // mongoDB/postgreSQL queries
+  }
+
+  markRHelpful: function(r_id) {
+    // mongoDB/postgreSQL queries
+  }
+
+  reportReview: function(r_id) {
+    // mongoDB/postgreSQL queries
+  }
+
+};
