2275f92f449b70304c51b69260914b447f0ac6ee
Roshan Patel <roshanupatel@gmail.com>
2022-11-11 17:54:06 -0800

    trying to get loader.io to work on my API server hosted by AWS

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 
 app.use(express.json());
 
+app.get("/loaderio-78fc410f27ea4c31e6c7a47c017470ed", (req, res) => {
+  res.send("loaderio-78fc410f27ea4c31e6c7a47c017470ed");
+});
+
 app.use('/api', router);
 const PORT = process.env.SERVER_PORT;
 const AWSDNSapi = process.env.AWS_DNS_API;
